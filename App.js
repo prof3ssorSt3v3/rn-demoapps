8c317f50aa547015b1c3a833f5c233cae4775958
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2021-12-05 21:00:26 -0500

    made changes to App.js and app.json for Location API

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
@@ -19,3 +41,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/**
+ * Permissions in app.json
+"android":{
+  "permissions": ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"]
+},
+"ios":{
+  "infoPlist": {
+        "NSLocationAlwaysUsageDescription": "We need to display your location on the home screen."
+      }
+}
+ * 
+ */
