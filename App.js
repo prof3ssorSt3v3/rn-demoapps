0e46019e155a26e9dd1dbaffd0cd54d079283425
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2021-12-05 21:27:21 -0500

    added Platform, Application, Network, and Cellular sample code

--- App.js
+++ App.js
@@ -1,8 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import * as Cellular from 'expo-cellular';
+import * as Network from 'expo-network';
+import * as Application from 'expo-application';
 
 export default function App() {
+  //The cellular methods
+  const getCellularStuff = async () => {
+    const allowVoip = await Cellular.allowsVoipAsync();
+    const carrierName = await Cellular.getCarrierNameAsync();
+    const cellGen = await Cellular.getCellularGenerationAsync(); //this is a number
+    const isoCountryCode = await Cellular.getIsoCountryCodeAsync();
+    const mobileCountryCode = await Cellular.getMobileCountryCodeAsync();
+    const mobileNetworkCode = await Cellular.getMobileNetworkCodeAsync();
+    const cellGenName = await Cellular.CellularGeneration[cellGen];
+    console.log(carrierName, isoCountryCode);
+    console.log(mobileCountryCode, mobileNetworkCode);
+    console.log(cellGen, cellGenName);
+  };
+
+  getCellularStuff();
+
+  //the network and application methods
+  const getNetworkStuff = async () => {
+    const ip = await Network.getIpAddressAsync();
+    const state = await Network.getNetworkStateAsync();
+    const isAirplane = await Network.isAirplaneModeEnabledAsync();
+    console.log(ip, state);
+    console.log({ isAirplane });
+
+    const appID = Application.applicationId;
+    const appName = Application.applicationName;
+    console.log(appID, appName); //Android application id, iOS bundle id
+    const version = Application.nativeApplicationVersion;
+    console.log('version', version);
+    const installedAt = await Application.getInstallationTimeAsync();
+    console.log(installedAt);
+
+    if (Platform.OS === 'ios') {
+      const releaseType = Application.getIosApplicationReleaseType();
+      console.log(Application.ApplicationReleaseType[releaseType]);
+    }
+  };
+  getNetworkStuff();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
