3f22c0d438e6dcbbe3c2b5eb8f294fab6954b1bc
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2021-12-07 08:51:27 -0500

    added demo animation with controls to move a view single direction and in a sequence

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  SafeAreaView,
+  Button,
+} from 'react-native';
 
 export default function App() {
+  const myAnim = useRef(new Animated.Value(0)).current;
+
+  const right = Animated.timing(myAnim, {
+    toValue: 100,
+    duration: 500,
+    useNativeDriver: true,
+  });
+  const left = Animated.timing(myAnim, {
+    toValue: 0,
+    delay: 500,
+    duration: 1000,
+    useNativeDriver: true,
+  });
+
+  const moveRight = () => {
+    right.start();
+  };
+  const moveLeft = () => {
+    left.start();
+  };
+  const moveSequence = () => {
+    Animated.sequence([right, left]).start();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.container, { padding: 10 }]}>
+        <Animated.View
+          style={{
+            transform: [{ translateX: myAnim }],
+            width: 200,
+            height: 200,
+            borderRadius: 10,
+            backgroundColor: 'khaki',
+          }}
+        >
+          <Text style={{ padding: 10 }}>Hello</Text>
+        </Animated.View>
+      </View>
+      <View style={styles.container}>
+        <Button title="Go Right" onPress={() => moveRight()} />
+        <Button title="Go Left" onPress={() => moveLeft()} />
+        <Button title="Go Both" onPress={() => moveSequence()} />
+      </View>
     </View>
   );
 }
