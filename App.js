6f43e8e0334797d4fa357dfb17fffeb7c46f1d78
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2021-12-07 10:43:42 -0500

    basic demo for using the Keyboard and KeyboardAvoidingView

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TextInput,
+  Pressable,
+} from 'react-native';
 
 export default function App() {
+  //possible values for behaviour are 'height', 'position', 'padding'
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <KeyboardAvoidingView
+      behaviour={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <Pressable onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.text}>This is a Label</Text>
+          <TextInput placeholder="Username" style={styles.textInput} />
+        </View>
+      </Pressable>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -14,6 +33,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  inner: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  text: {
+    fontSize: 30,
+  },
+  textInput: {
+    height: 40,
+    fontSize: 24,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+    marginBottom: 36,
   },
 });
