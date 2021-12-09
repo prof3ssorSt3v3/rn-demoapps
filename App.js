d600e44ecf7dc700b91fd504f4a13f174b8b2591
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2021-12-09 17:00:34 -0500

    demo complete for AsyncStorage useAsyncStorage hook

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [value, setValue] = useState([]);
+  const { getItem, setItem } = useAsyncStorage('AnotherKey');
+
+  const readItemFromStorage = () => {
+    getItem()
+      .then((item) => {
+        //get the value from AsyncStorage and save it in `value`
+        item = item === null ? [] : JSON.parse(item);
+        setValue(item);
+      })
+      .catch(console.log);
+  };
+
+  const addItemToStorage = (newValue) => {
+    //add the newValue to the array and overwrite it in AsyncStorage
+    setItem((currentArr) => [newValue, ...currentArr]);
+    //item in variable is an array
+    setValue(JSON.stringify([newValue, ...value]))
+      .then(() => {
+        console.log('saved new value in array');
+      })
+      .catch(console.log);
+    //item in AsyncStorage is a String representation of the Array
+  };
+
+  useEffect(() => {
+    readItemFromStorage();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Update value"
+        style={{ margin: 20 }}
+        onPress={() => {
+          let numString = Math.random().toString(36).substr(2, 5);
+          addItemToStorage(numString);
+        }}
+      />
+      <Text style={{ margin: 20 }}>Current value: {JSON.stringify(value)}</Text>
     </View>
   );
 }
